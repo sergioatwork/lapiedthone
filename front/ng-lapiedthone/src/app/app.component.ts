84e7e066f505479753f4cad96d75b5955683922d
Sergio At Work <sergio.atwork69@gmail.com>
2023-01-08 21:23:35 +0100

    Affichage Réseaux Sociaux en fonction de la route... Ca, c'était chaud !!! A te faire détester Angular...

--- front/ng-lapiedthone/src/app/app.component.ts
+++ front/ng-lapiedthone/src/app/app.component.ts
@@ -1,50 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router, UrlSegment } from '@angular/router';
-import { map, Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Router, } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
-  
-  constructor(
-    private routeActuelle: ActivatedRoute,
-    private router: Router) {
-      
-    }
 
-  title = 'La Piedthone';
+	private urlRoute: string = 'abc';
+	public pageAccueil: boolean = true;
 
-  
-  // private urlRoute: Observable<string> = this.routeActuelle.url.pipe(map(segments => segments.join('')));
-  private urlRoute: string = '';
-  public pageAccueil: boolean = true;
+	constructor(
+		private router: Router) { }
 
-  ngOnInit(): void {
-    
-    // this.routeActuelle.fragment.subscribe(url => { this.urlRoute = url ?? 'abc'; console.warn(url) });
-    // this.routeActuelle.snapshot.url[0].path;
-    // this.urlRoute = this.routeActuelle.toString() ?? 'abc';
-    this.urlRoute = this.router.routerState.snapshot.url;
-    console.log(this.urlRoute);
+	title = 'La Piedthone';
 
-    // // this.urlRoute = this.routeactuelle.snapshot.params;
-    // this.urlRoute = this.router.url;
-    // console.log(this.urlRoute);
-
-    // this.routeactuelle.queryParams.subscribe(params => {
-    //   this.urlRoute = params;
-
-    // console.log(this.urlRoute);
-    // });
-  }
-
-  affichagers() {
-    this.pageAccueil = !this.pageAccueil;
-    console.log(this.pageAccueil);
-  }
-
+	ngOnInit(): void {
+		this.router.events.subscribe(event => {
+			this.urlRoute = this.router.url.toString();
+			// console.log(this.urlRoute);
+			if (this.urlRoute.startsWith('/user')) { this.pageAccueil = false; } else { this.pageAccueil = true; }
+		});
+	}
 }
